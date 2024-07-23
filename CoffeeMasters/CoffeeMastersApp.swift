85d026d75c15c4e0d6df6c857a079f72780a3154
nadialvy <5026231007@student.its.ac.id>
2024-07-23 18:56:15 +0700

    Initial Commit

---
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeMastersApp.swift
+//  CoffeeMasters
+//
+//  Created by Nadia Lovely on 23/07/24.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeMastersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
