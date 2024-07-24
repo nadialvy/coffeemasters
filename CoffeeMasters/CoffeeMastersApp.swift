3d7efacc9a1fa1d54b94992143f969023f87f2c1
nadialvy <5026231007@student.its.ac.id>
2024-07-24 14:28:25 +0700

    feat: init projects

--- CoffeeMasters/CoffeeMastersApp.swift
+++ CoffeeMasters/CoffeeMastersApp.swift
@@ -8,10 +8,34 @@
 import SwiftUI
 
 @main
+// implementing/conforming the App (App is a protocol)
+// if we are conforming a protocol, we have to implementing the property of its protocol
 struct CoffeeMastersApp: App {
+//    dengan mendecalse some berarti kita harus mereturn 1 spesifik tipe tertentu
+//    coba pikirin, pernah ga di iphone kita running 2 apps di 1 screen secara bersamaan...
+//    ngga kan? nah thats why kita perlu keyword some
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
         }
     }
+}
+
+class Base {
+    
+}
+
+class A: Base {
+    
+}
+
+class B: Base {
+    
+}
+
+func createBase(index: Int) -> some Base{
+        return A()
+//        return B()
+    
 }
--- CoffeeMasters/ContentView.swift
+++ CoffeeMasters/ContentView.swift
@@ -8,14 +8,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView{
+            VStack {
+                
+            }
+
         }
-        .padding()
     }
 }
 
